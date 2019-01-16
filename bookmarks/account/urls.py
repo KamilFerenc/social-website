909c91ee14446be32a719ebeb22e595b409a7f45
KamilFerenc <kamilferenc91@gmail.com>
2019-01-16 13:46:32 +0100

    Create features for log in and log out

---
+++ bookmarks/account/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'account'
+urlpatterns = [
+    #url(r'login', views.user_login, name='login'),
+    url(r'^$', views.main, name='main'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard')
+]
