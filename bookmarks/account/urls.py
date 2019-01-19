427e6db3a20b8059ed4855184590e47911b4d724
KamilFerenc <kamilferenc91@gmail.com>
2019-01-19 11:26:32 +0100

    Add feature to register user

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     #url(r'login', views.user_login, name='login'),
     url(r'^$', views.main, name='main'),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^register/$', views.register, name='register'),
+
 ]
